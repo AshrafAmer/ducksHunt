186827a96226249174fd69313df3f1cd01101a93
Ashraf Amer <ashraf.amer55@gmail.com>
2020-02-14 16:00:48 +0200

    Update page2JS.js

--- js/page2JS.js
+++ js/page2JS.js
@@ -6,100 +6,118 @@ function random(min, max) {
 function createElement(tagName) {
     return document.createElement(tagName);
 }
-function reverse(position, duck, ducks) {
-    let increase = 10;
-    let timeSpeed = 10;
-    duck.classList.remove("d-none");
+/* ----------------------------------------- Start Mohamed Farag ------------------------------------------- */
+function createImg(position, source, ducks, color, level) {
+    let increase;
+    let timeSpeed;
+    if (level === "level-1") {
+        increase = 10;
+        timeSpeed = 20;
+    } else {
+        increase = 15;
+        timeSpeed = 10;
+    }
+    let scrWidth = $(window).width();
+    let duck = $("<img />", {
+        id: color,
+        class: `duck ${color} target fly`,
+        src: `../images/${source}`
+    });
     if (position === 0) {
-        duck.style.transform = "rotateY(180deg)";
-        duck.style.left = "-200px";
-        duck.style.top = random(0, parseInt(ducks.style.height) - 200) + "px";
+        $(duck).css({
+            transform: "rotateY(180deg)",
+            left: "-200px",
+            top: random(0, parseInt($(ducks).height()) - 200) + "px"
+        });
         let move = setInterval(function() {
-            duck.style.left = parseInt(duck.style.left) + increase + "px";
-            if (parseInt(duck.style.left) > window.innerWidth) {
+            $(duck).css("left", `+=${increase}px`);
+            if (parseInt($(duck).css("left")) > scrWidth) {
+                $(duck).remove();
                 clearInterval(move);
             }
         }, timeSpeed);
     } else {
-        duck.style.right = "-200px";
-        duck.style.top = random(0, parseInt(ducks.style.height) - 200) + "px";
+        $(duck).css({
+            right: "-200px",
+            top: random(0, parseInt($(ducks).height()) - 200) + "px"
+        });
         let move = setInterval(function() {
-            duck.style.right = parseInt(duck.style.right) + increase + "px";
-            if (parseInt(duck.style.right) > window.innerWidth) {
+            $(duck).css("right", `+=${increase}px`);
+            if (parseInt($(duck).css("right")) > scrWidth) {
+                $(duck).remove();
                 clearInterval(move);
             }
         }, timeSpeed);
     }
+    $(duck).prependTo($(ducks));
 }
-
+/* ----------------------------------------- end Mohamed Farag ------------------------------------------- */
+// start Ashraf
+let moveBomb;
+function bombInterval(ducks) {
+    /* Bomb Code */
+    let scrHeight = $(window).height();
+    moveBomb = setInterval(function() {
+        //Creation
+        let bombImg = $(
+            `<img class="bomb target" style="left:${Math.random() * 1000}px">`
+        );
+        bombImg.attr("src", "../images/bomb.png");
+        bombImg.appendTo(ducks);
+        let move = setInterval(function() {
+            //Move
+            bombImg.css("top", "+=1px");
+            if (
+                parseInt(bombImg.css("top") + bombImg.css("width")) > scrHeight
+            ) {
+                bombImg.remove();
+                clearInterval(move);
+            }
+        }, 1); //bombs
+    }, 15000); //[endCode]
+}
+// end Ashraf
+/* ----------------------------------------- Start Mohamed Farag ------------------------------------------- */
 function interval(ducks, name, _levelType, _player, _score) {
-    let minute = 1;
-    let second = 0;
-    let speed = 0;
+    // start Ashraf
     let startScore = _score;
     let currentPlayerName = _player;
     let levelType = _levelType;
+    // end Ashraf
+    // Start Mohamed sabry elnagar
+    let minute;
+    let second;
+    let speed;
     if (levelType === "level-1") {
-        speed = 500;
-    } else if (levelType === "level-2") {
-        speed = 850;
-        var moveBomb = setInterval(function() {
-            //Creation
-            let bombImg = $(
-                `<img class="bomb" style="right:${Math.random() * 1000}px;">`
-            );
-            bombImg.attr("src", "../images/bomb.png");
-            bombImg.appendTo(ducks);
-
-            bombImg.on("click", function() {
-                bombImg.attr("src", "../images/bombAfter.png");
-                bombImg.addClass("bombAfter");
-                setTimeout(function() {
-                    bombImg.remove();
-                }, 150);
-            });
-
-            if (bombImg) {
-                setTimeout(function() {
-                    bombImg.remove();
-                }, 5000);
-            }
-        }, 15000);
-
-        setInterval(function() {
-            //Move
-            $(".bomb").css("top", "+=1px");
-        }, 1); //bombs [endCode]
+        minute = 1;
+        second = 0;
+        speed = 1000;
+    } else {
+        minute = 2;
+        second = 0;
+        speed = 300;
     }
+    // end Mohamed sabry elnagar
+
+    let imgSource = ["duckWhite.gif", "duckBlack.gif", "duckGold.gif"];
     let moveDucks = setInterval(function() {
         let position = random(0, 1);
-        let duck = createElement("img");
         let randomColor = random(0, 2);
-        let newDuck = "";
-        duck.style.position = "absolute";
         switch (randomColor) {
             case 0:
-                newDuck = $("#white").clone(true);
-                reverse(position, newDuck[0], ducks);
-                ducks.append(newDuck[0]);
+                createImg(position, imgSource[0], ducks, "white", levelType);
                 break;
             case 1:
-                newDuck = $("#black").clone(true);
-                reverse(position, newDuck[0], ducks);
-                ducks.append(newDuck[0]);
+                createImg(position, imgSource[1], ducks, "black", levelType);
                 break;
             case 2:
-                newDuck = $("#gold").clone(true);
-                reverse(position, newDuck[0], ducks);
-                ducks.append(newDuck[0]);
+                createImg(position, imgSource[2], ducks, "gold", levelType);
                 break;
             default:
             // code block
         }
-        setTimeout(function() {
-            newDuck[0].remove();
-        }, 2000);
     }, speed);
+    // Start Mohamed sabry elnagar
     let timers = setInterval(function() {
         second--;
         if (second >= 0) {
@@ -135,7 +153,7 @@ function interval(ducks, name, _levelType, _player, _score) {
             if (levelType === "level-2") {
                 clearInterval(moveBomb);
             }
-
+            /* Start Ashraf */
             /* Check Winner */
             let currentScore = JSON.parse(localStorage.getItem("player")).find(
                 function(item) {
@@ -145,7 +163,6 @@ function interval(ducks, name, _levelType, _player, _score) {
                     );
                 }
             ).score;
-
             if (currentScore - startScore >= 200) {
                 //winner
                 startScore = currentScore;
@@ -215,10 +232,14 @@ function interval(ducks, name, _levelType, _player, _score) {
                     }
                 }
             );
+            /* end Ashraf */
         }
     }, 1000);
+    // End Mohamed sabry elnagar
 }
+/* ----------------------------------------- end Mohamed Farag ------------------------------------------- */
 window.addEventListener("load", function() {
+    // start Mohamed sabry elnagar
     let namePlayer = document.getElementById("namePlayer");
     let level = document.getElementById("level");
     let scoreText = document.getElementById("scoreText");
@@ -237,15 +258,15 @@ window.addEventListener("load", function() {
             item.playerName === objStrPlayerName && item.level === objStrLevel
         );
     });
-    ducks.style.width = window.innerWidth + "px";
-    ducks.style.height = window.innerHeight - 91 + "px";
+    ducks.style.width = $(window).width() + "px";
+    ducks.style.height = $(window).height() - 91 + "px";
     window.addEventListener("resize", function() {
-        ducks.style.width = window.innerWidth + "px";
-        ducks.style.height = window.innerHeight - 91 + "px";
+        ducks.style.width = $(window).width() + "px";
+        ducks.style.height = $(window).height() - 91 + "px";
     });
     swal(
         {
-            title: "Are You ready to start?!",
+            title: "Are you ready to start ?!",
             type: "info",
             showCancelButton: true,
             confirmButtonClass: "btn-primary",
@@ -272,7 +293,7 @@ window.addEventListener("load", function() {
                             level.innerText = search.level;
                             scoreText.innerText = "Score ";
                             score.innerText = search.score;
-                            timer.innerText = "Timer 02 : 00";
+                            timer.innerText = "Timer 01 : 00";
                             interval(
                                 ducks,
                                 search.playerName,
@@ -280,6 +301,10 @@ window.addEventListener("load", function() {
                                 objStrPlayerName,
                                 search.score
                             );
+                            if (objStrLevel === "level-2") {
+                                timer.innerText = "Timer 02 : 00";
+                                bombInterval(ducks);
+                            }
                         }
                     }
                 );
@@ -299,39 +324,59 @@ window.addEventListener("load", function() {
                     }
                 );
             }
+            // end Mohamed sabry elnagar
         }
     );
+    /* ----------------------------------------- Start Mohamed Farag ------------------------------------------- */
     ducks.addEventListener("mousedown", function(e) {
+        let target = e.target;
+        // start Mohamed sabry elnagar
         audio.currentTime = 0;
         audio.play();
-        console.log(e.target.classList[0]);
-        if (e.target.classList[0] === "duck") {
-            duckDie(e.target, score);
+        // end Mohamed sabry elnagar
+        if (target.classList[0] === "duck") {
+            duckDie(target, score);
             // click on bomb
-        } else if (e.target.classList[0] === "bomb") {
+        } else if (target.classList[0] === "bomb") {
             // get near Ducks
             let nearDucks = $("img.duck");
+            let bombLeft = parseInt($(e.target).css("left"));
+            let bombTop = parseInt($(e.target).css("top"));
+            // let bombWidth = parseInt($(e.target).css("width"));
+            // let bombHeight = parseInt($(e.target).css("height"));
+
             for (let i = 0; i < nearDucks.length; i++) {
                 let duckLeft = parseInt($(nearDucks[i]).css("left"));
-                let duckWidth = parseInt($(nearDucks[i]).css("width"));
-                let duckHeight = parseInt($(nearDucks[i]).css("height"));
+                // let duckWidth = parseInt($(nearDucks[i]).css("width"));
+                // let duckHeight = parseInt($(nearDucks[i]).css("height"));
                 let duckTop = parseInt($(nearDucks[i]).css("top"));
-                let bombLeft = parseInt($(e.target).css("left"));
-                let bombTop = parseInt($(e.target).css("top"));
-                let bombWidth = parseInt($(e.target).css("width"));
-                let bombHeight = parseInt($(e.target).css("height"));
+                // console.log(
+                //     Math.abs(bombLeft - duckLeft),
+                //     Math.abs(bombTop - duckTop),
+                //     Math.abs(duckLeft - bombLeft),
+                //     Math.abs(duckTop - bombTop)
+                // );
                 if (
-                    bombLeft - (duckLeft + duckWidth) <= 50 || // left
-                    bombTop - (duckTop + duckHeight) <= 50 || // top
-                    duckLeft - (bombLeft + bombWidth) <= 50 || //right
-                    duckTop - (bombTop + bombHeight) <= 50 // bottom
+                    Math.abs(bombLeft - duckLeft) < 50 || // left
+                    Math.abs(bombTop - duckTop) < 50 || // top
+                    Math.abs(duckLeft - bombLeft) < 50 || //right
+                    Math.abs(duckTop - bombTop) < 50 // bottom
                 ) {
                     duckDie(nearDucks[i], score);
                 }
             }
+            // start Mohamed sabry elnagar
             audioExplosion.currentTime = 0;
             audioExplosion.play();
+            // end Mohamed sabry elnagar
+            $(target).attr("src", "../images/bombAfter.png");
+            $(target).addClass("bombAfter");
+            setTimeout(function() {
+                $(target).remove();
+            }, 500);
         }
+        // end Ashraf
+        // start Mohamed sabry elnagar
         let objNew = {
             playerName: namePlayer.innerText,
             level: level.innerText,
@@ -347,9 +392,9 @@ window.addEventListener("load", function() {
         obj[searchItem] = objNew;
         let objString = JSON.stringify(obj);
         localStorage.setItem("player", objString);
+        // end Mohamed sabry elnagar
     });
 });
-
 function duckDie(ducks, score) {
     let heightScr = $(window).height();
     ducks.src = "../images/die.png";
@@ -375,4 +420,6 @@ function duckDie(ducks, score) {
         default:
         // code block
     }
+    // end Mohamed sabry elnagar
 }
+/* ----------------------------------------- End Mohamed Farag ------------------------------------------- */
